00b10e182b2cd0040bc93cfcc8ec00c38e58e760
Demetrio K <dimo.ruso@gmail.com>
2022-10-03 14:08:52 -0300

    desafio 12 - orden de compra

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CarContext";
@@ -6,6 +7,28 @@ import ItemCart from "../ItemCart/ItemCart";
 
 const Cart = () => {
 	const { cart, totalPrice } = useCartContext();
+
+	const order = {
+		buyer: {
+			name: "Dimo Russ",
+			email: "dimoruss@gmail.com",
+			phone: "123123",
+			address: "asdd",
+		},
+		items: cart.map((product) => ({
+			id: product.id,
+			title: product.title,
+			price: product.price,
+			quantity: product.quantity,
+		})),
+		total: totalPrice(),
+	};
+
+	const handleClick = () => {
+		const db = getFirestore();
+		const ordersCollection = collection(db, "orders");
+		addDoc(ordersCollection, order).then(({ id }) => console.log(id));
+	};
 
 	if (cart.length === 0) {
 		return (
